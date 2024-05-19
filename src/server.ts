fc71cbd13b397ddd5ccc48598eed36e4f0d0e3e3
coder7475 <robiulhossain7475@gmail.com>
2024-05-19 07:00:32 +0600

    feat: add parsers to app

--- src/server.ts
+++ src/server.ts
@@ -5,11 +5,16 @@ import app from "./app";
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect(config.database_url as string);
+  try {
+    await mongoose.connect(config.database_url as string);
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`)
+    })
 
-  app.listen(config.port, () => {
-    console.log(`Example app listening on port ${config.port}`)
-  })
+  } catch(err) {
+    console.log(err);
+  }
+
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
