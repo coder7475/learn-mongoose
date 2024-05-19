824caa66cef24d93d20530a5f7a32a5ad635df2b
coder7475 <robiulhossain7475@gmail.com>
2024-05-19 07:53:36 +0600

    feat: setup eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,30 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ['src/**/*.ts', 'src/**/*.tsx'],
+    languageOptions: {
+      globals: globals.node,
+      // ecmaVersion: 'latest',
+      sourceType: 'module',
+      parser: tsParser,
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+    },
+  },
+  {
+    ignores: ["node_modules", "dist"]
+  }
 ];
