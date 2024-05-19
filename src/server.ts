ac6794058a36ac9637f2ae0544ca1623b107a439
coder7475 <robiulhossain7475@gmail.com>
2024-05-19 21:00:31 +0600

    feat: update prettier config

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
-import mongoose from 'mongoose'
-import config from './app/config'
-import app from './app'
+import mongoose from 'mongoose';
+import config from './app/config';
+import app from './app';
 
-main().catch(err => console.log(err))
+main().catch(err => console.log(err));
 
 async function main() {
   try {
-    await mongoose.connect(config.database_url as string)
+    await mongoose.connect(config.database_url as string);
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`)
-    })
+      console.log(`Example app listening on port ${config.port}`);
+    });
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
