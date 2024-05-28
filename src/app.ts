b2bb29aa89fbcb3df63fe7282dcfa6045772e1fb
coder7475 <robiulhossain7475@gmail.com>
2024-05-28 22:40:42 +0600

    feat: basic eslint setup

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,15 @@
-import cors from 'cors'
-import express, { Application, Request, Response } from 'express'
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
 
-const app: Application = express()
+const app: Application = express();
 
 // parsers
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
-  // const a = 10;
-
-  res.send('Hello World!')
-})
+  res.send('Exp App is Running!');
+});
 
 // console.log(process.cwd());
-export default app
+export default app;
